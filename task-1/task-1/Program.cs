6fa5a892be7dd2b8a2b89dd6d28a0b0e268363d7
ismayilzeynal <ismayilbz@code.edu.az>
2022-08-03 17:20:38 +0400

    tasks added

---
+++ task-1/task-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 8, 11, 99, 66, -44, 123, 546, 7486, 132, -4 };
+            int mini = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < arr[mini])
+                    mini = i;
+            }
+            Console.WriteLine($"min element found at index {mini} : {arr[mini]}");
+        }
+    }
+}
---
+++ task-2/task-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            bool found = false;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == 'a')
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+                Console.WriteLine("letter 'a' found");
+            else
+                Console.WriteLine("not found");
+        }
+    }
+}
